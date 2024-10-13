f188dcd3bb7a983b875847277bcc7d0122405be7
slivchuk-ts <taniaslivchuk97@gmail.com>
2024-10-13 22:37:53 +0200

    add hw

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ContactForm from './components/ContactForm/ContactForm';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactList from './components/ContactList/ContactList';
+import s from './App.module.css';
+
+export default function App() {
+
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
